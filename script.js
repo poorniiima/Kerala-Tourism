cdcd5f517916aa8162a17127782b1818137048b7
poorni <poornimamani.mec@gmail.com>
2021-01-27 15:51:21 +0530

    Added all the files

---
+++ script.js
@@ -0,0 +1,12 @@
+let email=document.getElementById("email");
+// let pwd=document.getElementById("pwd");
+let emailerror = document.getElementById("emailerror");
+function ValidateEmail() 
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
